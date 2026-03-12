07d2054138d72ca8e701891048e07ad70145a5e1
BossDols <185933672+zel-asp@users.noreply.github.com>
2026-03-12 14:26:20 +0800

    d

--- core/Class/Database.php
+++ core/Class/Database.php
@@ -9,20 +9,24 @@ class Database
     private $statement;
     private $connection;
 
-    public function __construct($config, $username = 'if0_41364870', $password = 'N2yKRiTrARDe')
+    public function __construct($config, $username = null, $password = null)
     {
+        // Use the username and password from config if not provided
+        $username = $username ?? $config['username'];
+        $password = $password ?? $config['password'];
+
         $dsn = 'mysql:' . http_build_query($config, '', ';');
+
         $this->connection = new PDO($dsn, $username, $password, [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION // Add this for better error reporting
         ]);
     }
 
     public function query($query, $param = [])
     {
         $this->statement = $this->connection->prepare($query);
-
         $this->statement->execute($param);
-
         return $this;
     }
 
@@ -54,7 +58,6 @@ class Database
     public function fetch_one()
     {
         return $this->statement->fetch();
-
     }
 
     public function lastInsertId()
