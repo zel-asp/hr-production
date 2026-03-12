c90fa0cf4e16af34d4dfc7e8d8477f529ebfda31
BossDols <185933672+zel-asp@users.noreply.github.com>
2026-03-12 14:23:21 +0800

    f

--- config/config.php
+++ config/config.php
@@ -1,12 +1,11 @@
 <?php
-
 return [
     'database' => [
-        'host' => 'sql101.infinityfree.com',
-        'port' => '3306',
-        'dbname' => 'if0_41364870_hr_database',
-        'charset' => 'utf8mb4',
-        'username' => 'if0_41364870',
-		'password' => 'N2yKRiTrARDe'
+        'host' => 'gondola.proxy.rlwy.net',
+        'port' => '43824',
+        'dbname' => 'railway',
+        'username' => 'root',
+        'password' => 'PFehwhnPqYpMrSswkAQYgWmWJQHPrQIp',
+        'charset' => 'utf8mb4'
     ]
 ];
